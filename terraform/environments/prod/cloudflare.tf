d53b4f15a4c56e668dbbb93eb4268c81d1083873
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-06-01 11:11:23 +0200

    Fix config

--- terraform/environments/prod/cloudflare.tf
+++ terraform/environments/prod/cloudflare.tf
@@ -6,16 +6,18 @@ data "cloudflare_zone" "main" {
 
 resource "cloudflare_dns_record" "this" {
   type = "A"
-  zone_id = data.cloudflare_zone.main.id
+  zone_id = data.cloudflare_zone.main.zone_id
   name = "@"
   proxied = true
   ttl = 1
+  content = hcloud_load_balancer.this.ipv4
 }
 
 resource "cloudflare_dns_record" "wildcard" {
   type = "A"
-  zone_id = data.cloudflare_zone.main.id
+  zone_id = data.cloudflare_zone.main.zone_id
   name = "*"
   proxied = true
   ttl = 1
+  content = hcloud_load_balancer.this.ipv4
 }
