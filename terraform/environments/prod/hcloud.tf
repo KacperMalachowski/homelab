4152186f7a7f2131e67e1acd8488fb353e697065
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-07-19 08:53:37 +0200

    Update domain

--- terraform/environments/prod/hcloud.tf
+++ terraform/environments/prod/hcloud.tf
@@ -3,25 +3,11 @@ locals {
     "0.0.0.0/0",
     "::/0"
   ]
-
-  public_domain = "malachowski.me"
-  private_domain = "malachowski.local"
-}
-
-resource "null_resource" "domain_name" {
-  # This resource is used to trigger the Talos module to update the domain name
-  # when the public or private domain changes.
-  triggers = {
-    public_domain  = local.public_domain
-    private_domain = local.private_domain
-  }
 }
 
 module "talos" {
   source = "hcloud-talos/talos/hcloud"
   version = "2.15.13"
-
-  depends_on = [ null_resource.domain_name ]
 
   talos_version = "v1.9.5"
   kubernetes_version = "1.30.3"
@@ -29,9 +15,9 @@ module "talos" {
 
   hcloud_token = var.hcloud_token
 
-  cluster_name = local.public_domain
-  cluster_domain = local.private_domain
-  cluster_api_host = "kube.${local.public_domain}"
+  cluster_name = var.public_domain
+  cluster_domain = var.private_domain
+  cluster_api_host = "kube.${var.public_domain}"
   enable_alias_ip = true
   output_mode_config_cluster_endpoint = "cluster_endpoint"
 
