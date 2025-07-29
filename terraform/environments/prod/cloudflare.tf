b53b28cdb2a92ecfc48d1921f5618231f9fb5b5d
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-07-29 18:55:17 +0200

    Remove kube

--- terraform/environments/prod/cloudflare.tf
+++ terraform/environments/prod/cloudflare.tf
@@ -13,15 +13,6 @@ resource "cloudflare_dns_record" "this" {
   content = module.masters.hcloud_servers[0].ipv4_address
 }
 
-resource "cloudflare_dns_record" "kube" {
-  type    = "A"
-  zone_id = data.cloudflare_zone.main.zone_id
-  name    = "kube"
-  proxied = true
-  ttl     = 1
-  content = module.masters.hcloud_servers[0].ipv4_address
-}
-
 resource "cloudflare_dns_record" "wildcard" {
   type    = "A"
   zone_id = data.cloudflare_zone.main.zone_id
