1187d9a3dda7e3883f3b6c9d7444a97a1ec9d1f6
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-08-04 22:49:10 +0200

    Merge

--- terraform/environments/prod/prometheus.tf
+++ terraform/environments/prod/prometheus.tf
@@ -3,27 +3,16 @@ locals {
   prometheus_namespace    = "monitoring"
 }
 
-resource "helm_release" "prometheus" {
+resource "helm_release" "promstack" {
   name             = local.prometheus_release_name
   namespace        = local.prometheus_namespace
   create_namespace = true
   repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
-  version          = "27.29.0"
+  chart            = "kube-prometheus-stack"
+  version          = "75.15.2"
   timeout          = 800 # 10 minutes
 
   # If values file specified by the var.values_file input variable exists then apply the values from this file
   # else apply the default values from the chart
-  values = [fileexists("${path.root}/${var.prometheus_values_file}") == true ? file("${path.root}/${var.prometheus_values_file}") : ""]
-
-  set = [
-    {
-      name  = "persistence.storageClass"
-      value = var.prometheus_storageclass_name
-    },
-    {
-      name  = "persistence.size"
-      value = var.prometheus_storage_size
-    }
-  ]
+  values = [fileexists("${path.root}/${var.promstack_values_file}") == true ? file("${path.root}/${var.promstack_values_file}") : ""]
 }
