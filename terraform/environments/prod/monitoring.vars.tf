f77754222c306a241453cb523ce2753b8457d3f6
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-08-04 22:51:15 +0200

    Fix tflint

--- terraform/environments/prod/monitoring.vars.tf
+++ terraform/environments/prod/monitoring.vars.tf
@@ -1,14 +1,3 @@
-variable "prometheus_storageclass_name" {
-  description = "PVC Storage Class"
-  type        = string
-  default     = ""
-}
-
-variable "prometheus_storage_size" {
-  type        = string
-  description = "PVC Storage Size"
-  default     = "10Gi"
-}
 
 variable "promstack_values_file" {
   type        = string
