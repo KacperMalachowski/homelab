dd9db2c14d5a6bd1a307735b2e0d24d1a440974e
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-08-05 07:04:53 +0200

    feat: expose grafana

--- terraform/environments/prod/monitoring.tf
+++ terraform/environments/prod/monitoring.tf
@@ -1,11 +1,11 @@
 locals {
-  prometheus_release_name = "prometheus"
-  prometheus_namespace    = "monitoring"
+  promstack_release_name = "prometheus"
+  promstack_namespace    = "monitoring"
 }
 
 resource "helm_release" "promstack" {
-  name             = local.prometheus_release_name
-  namespace        = local.prometheus_namespace
+  name             = local.promstack_release_name
+  namespace        = local.promstack_namespace
   create_namespace = true
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
@@ -15,4 +15,38 @@ resource "helm_release" "promstack" {
   # If values file specified by the var.values_file input variable exists then apply the values from this file
   # else apply the default values from the chart
   values = [fileexists("${path.root}/${var.promstack_values_file}") == true ? file("${path.root}/${var.promstack_values_file}") : ""]
+}
+
+
+resource "kubernetes_manifest" "grafana_dashboard_ingressroute" {
+  depends_on = [helm_release.promstack]
+
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1",
+    kind       = "IngressRoute",
+    metadata = {
+      name      = "grafana",
+      namespace = local.promstack_namespace
+    },
+    spec = {
+      entryPoints = ["websecure"],
+      routes = [
+        {
+          kind     = "Rule",
+          match    = "Host(`grafana.${var.public_domain}`)",
+          priority = 10
+          services = [
+            {
+              name = "prometheus-grafana",
+              port = 80
+            }
+          ]
+        }
+      ]
+      tls = {
+        certResolver = "default"
+      }
+    }
+  }
+
 }
