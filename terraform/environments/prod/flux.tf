0c49a5473764c7a2346aa958c44413f0eba6b7a6
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-08-08 08:40:50 +0200

    USe flux

---
+++ terraform/environments/prod/flux.tf
@@ -0,0 +1,22 @@
+data "github_repository" "manifests" {
+  full_name = "${var.github_organization}/${var.github_manifest_repository}"
+}
+
+resource "tls_private_key" "flux_deploy_key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "github_repository_deploy_key" "flux" {
+  repository = data.github_repository.manifests.name
+  title      = "flux"
+  key        = tls_private_key.flux_deploy_key.public_key_openssh
+  read_only  = false
+}
+
+resource "flux_bootstrap_git" "prod" {
+  depends_on = [github_repository_deploy_key.flux, module.kube_hetzner]
+
+  embedded_manifests = true
+  path               = "clusters/prod/flux-system"
+}
--- terraform/environments/prod/providers.tf
+++ terraform/environments/prod/providers.tf
@@ -23,3 +23,23 @@ provider "kubernetes" {
   client_certificate     = module.kube_hetzner.kubeconfig_data.client_certificate
 
 }
+
+provider "flux" {
+  kubernetes = {
+    host = module.kube_hetzner.kubeconfig_data.host
+
+    cluster_ca_certificate = module.kube_hetzner.kubeconfig_data.cluster_ca_certificate
+    client_key             = module.kube_hetzner.kubeconfig_data.client_key
+    client_certificate     = module.kube_hetzner.kubeconfig_data.client_certificate
+  }
+
+  git = {
+    url = "ssh://git@github.com/${var.github_organization}/${var.github_manifest_repository}.git"
+
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux_deploy_key.private_key_openssh
+    }
+  }
+
+}
